752d867140e69a32737033129a9e676d1614c3b2
t115liyanpeng <t115liyanpeng@126.com>
2020-04-10 12:50:19 +0800

    添加数据库操作类

--- swsqlx/sql.go
+++ swsqlx/sql.go
@@ -10,11 +10,48 @@ import (
 )
 
 // Dbc mysql 连接
-var Dbc *sql.Conn
+var Dbc *MysqlDbc
 
 // InitedMysql 初始化mysql连接
-func InitedMysql(dbcfg *conf.Databasecfg) error {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbcfg.User, dbcfg.Password, dbcfg.IPAddress, dbcfg.Port, dbcfg.DbName)
-	Dbc, err := sql.Open("mysql", dsn)
-	return err
+func initedMysql(dbcfg *conf.Databasecfg) (dbconn *sql.DB, err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbcfg.User, dbcfg.Password, dbcfg.IPAddress, dbcfg.Port, dbcfg.DbName)
+	db, err := sql.Open("mysql", dsn)
+	return db, err
+}
+
+//MysqlDbc mysql 数据库连接实例
+type MysqlDbc struct {
+	sqlDb *sql.DB
+}
+
+//NewMysqlDbc 构造函数
+func NewMysqlDbc(dbcfg *conf.Databasecfg) (dbc *MysqlDbc, err error) {
+	db, err := initedMysql(dbcfg)
+	dbc = &MysqlDbc{
+		sqlDb: db,
+	}
+	return dbc, err
+}
+
+//CreateDbcInstance 创建dbc实例
+func CreateDbcInstance(dbcfg *conf.Databasecfg) *MysqlDbc {
+	if nil == Dbc {
+		db, err := initedMysql(dbcfg)
+		if err == nil {
+			dbc := &MysqlDbc{
+				sqlDb: db,
+			}
+			Dbc = dbc
+		}
+
+	}
+	return Dbc
+
+}
+
+//ExecNonQuery 无返回
+func (dbc *MysqlDbc) ExecNonQuery() {
+	if dbc != nil {
+		dbc.sqlDb.Ping()
+	}
 }
