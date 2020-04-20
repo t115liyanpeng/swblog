efaa767b02dea4e2acf6f59e4d424d5151ee6570
t115liyanpeng <t115liyanpeng@126.com>
2020-04-20 19:27:48 +0800

    添加首页模型

---
+++ models/page/indexpage.go
@@ -0,0 +1,12 @@
+package page
+
+//UserModule 右上角用户信息
+type UserModule struct {
+	UserName string //显示的首页的用户的名称
+	Brief    string //个性签名
+	Email    string //邮件地址
+}
+
+//FirstPage 首页对应实体类
+type FirstPage struct {
+}
